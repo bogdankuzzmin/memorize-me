6f4ebc085eca3a6b253118c965b436e04988a4d2
Bogdan <bogdankuzzmin@gmail.com>
2021-04-21 21:18:11 +0600

    added cards structure, basic utils for cards

---
+++ src/constants.js
@@ -0,0 +1,157 @@
+export const HEARTS = [
+  {
+    rank: '6',
+    suit: 'heart',
+  },
+  {
+    rank: '7',
+    suit: 'heart',
+  },
+  {
+    rank: '8',
+    suit: 'heart',
+  },
+  {
+    rank: '9',
+    suit: 'heart',
+  },
+  {
+    rank: '10',
+    suit: 'heart',
+  },
+  {
+    rank: 'J',
+    suit: 'heart',
+  },
+  {
+    rank: 'Q',
+    suit: 'heart',
+  },
+  {
+    rank: 'K',
+    suit: 'heart',
+  },
+  {
+    rank: 'A',
+    suit: 'heart',
+  }
+];
+
+export const CLUBS = [
+  {
+    rank: '6',
+    suit: 'club',
+  },
+  {
+    rank: '7',
+    suit: 'club',
+  },
+  {
+    rank: '8',
+    suit: 'club',
+  },
+  {
+    rank: '9',
+    suit: 'club',
+  },
+  {
+    rank: '10',
+    suit: 'club',
+  },
+  {
+    rank: 'J',
+    suit: 'club',
+  },
+  {
+    rank: 'Q',
+    suit: 'club',
+  },
+  {
+    rank: 'K',
+    suit: 'club',
+  },
+  {
+    rank: 'A',
+    suit: 'club',
+  }
+];
+
+export const DIAMONDS = [
+  {
+    rank: '6',
+    suit: 'diamond',
+  },
+  {
+    rank: '7',
+    suit: 'diamond',
+  },
+  {
+    rank: '8',
+    suit: 'diamond',
+  },
+  {
+    rank: '9',
+    suit: 'diamond',
+  },
+  {
+    rank: '10',
+    suit: 'diamond',
+  },
+  {
+    rank: 'J',
+    suit: 'diamond',
+  },
+  {
+    rank: 'Q',
+    suit: 'diamond',
+  },
+  {
+    rank: 'K',
+    suit: 'diamond',
+  },
+  {
+    rank: 'A',
+    suit: 'diamond',
+  }
+];
+
+export const SPADES = [
+  {
+    rank: '6',
+    suit: 'spade',
+  },
+  {
+    rank: '7',
+    suit: 'spade',
+  },
+  {
+    rank: '8',
+    suit: 'spade',
+  },
+  {
+    rank: '9',
+    suit: 'spade',
+  },
+  {
+    rank: '10',
+    suit: 'spade',
+  },
+  {
+    rank: 'J',
+    suit: 'spade',
+  },
+  {
+    rank: 'Q',
+    suit: 'spade',
+  },
+  {
+    rank: 'K',
+    suit: 'spade',
+  },
+  {
+    rank: 'A',
+    suit: 'spade',
+  }
+];
+
+export const allCards = HEARTS.concat(CLUBS).concat(DIAMONDS).concat(SPADES);
