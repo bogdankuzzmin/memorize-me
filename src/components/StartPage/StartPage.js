870e1f9f97c8cc2e02411da715875f234ae96eb2
Bogdan <bogdankuzzmin@gmail.com>
2021-04-22 23:36:11 +0600

    [StartPage]: - added link to result component - added value to input - worked on styles

--- src/components/StartPage/StartPage.js
+++ src/components/StartPage/StartPage.js
@@ -1,20 +1,26 @@
+import {Link} from 'react-router-dom';
+
 import './StartPage.css';
 
 const startPage = props => {
-
   return (
     <section className="start-page">
       <h1 className="start-page__title">Memorize Me</h1>
 
-      <form className="start-page__form" onSubmit={(event) => props.submitHandler(event)}>
-        <input type="text" 
-               className="start-page__input"
-               placeholder="Type your name" 
-               onChange={(event) => props.changePlayerNameHandler(event)}
-               required />
+      <div className="start-page__wrapper">
+        <form className="start-page__form" onSubmit={(event) => props.submitHandler(event)}>
+          <input type="text" 
+                 className="start-page__input"
+                 placeholder="Type your name" 
+                 onChange={(event) => props.changePlayerNameHandler(event)}
+                 value={props.nameValue}
+                 required />
 
-        <button type="submit" className="start-page__button">Start</button>
-      </form>
+          <button type="submit" className="start-page__button">Start</button>
+        </form>
+
+        <Link to="/results" className="start-page__button start-page__button--second">Results</Link>
+      </div>
     </section>
   );
 };
